71cb8dba4c1cc18a5550e3944bdebb222678f84a
Srinivas32054 <srinusimhadri21980@gmail.com>
2026-08-14 12:07:10 +0530

    shell script

---
+++ 08-install-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+    echo "ERROR:: You must have sudo access"
+    exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0]
+then
+    dnf install mysql -y
+    if [ $? -ne 0]
+    then
+        echo "Installing mysql ... FAILURE"
+        exit 1
+    else
+        echo "Installing mysql ... SUCCESS"
+    fi
+else
+    echo "Mysql is aalready ... Installed"
+fi
