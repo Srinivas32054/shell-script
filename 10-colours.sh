f45ef15955994322157982e866ce1073787ea7d0
Srinivas32054 <srinusimhadri21980@gmail.com>
2026-08-17 12:52:07 +0530

    session 11

---
+++ 10-colours.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
+VALIDATE(){
+     if [ $1 -ne 0]
+    then
+        echo  -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access"
+    exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing Mysql"
+else
+    echo -e "Mysql is aalready ... $Y Installed"
+fi
